be26fc317e07b0a9e3c5e72f4ad3d7c35ecfdd03
samimulaw <samimulaw@gmail.com>
2017-10-25 01:50:42 -0500

    I am able to get data from the server I will display them as component tomorrow and work on modulariz after the integration

--- src/app/movie.component.ts
+++ src/app/movie.component.ts
@@ -1,80 +1,16 @@
-import { Component } from '@angular/core';
-import {Movie } from './movie'
+import { Component} from '@angular/core';
+import {Movie } from './movie';
+import {MovieService } from './movie.service';
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
 @Component({
   selector: 'movie-list',
-  template: `    
-  <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Movie Rent</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li (click)="movietodisplay= listMovie" ><a href="#">Movies</a></li>
-      <li ><a href="#">Add Movies</a></li>    
-          
-    </ul>
-  </div>
-</nav>
-<form #search="ngForm" (ngSubmit)="find(search)">
-<div class="row">
-<div class="col-lg-6">
-  <div class="input-group">
-    
-  </div>
-</div>
-<div class="col-lg-6">
-  <div class="input-group">
-    <input [(ngModel)]= "searchtitle"  name="search" #search="ngModel" id="movie_title"  type="text" class="form-control" placeholder="Search for...">
-    <span class="input-group-btn">
-      <button class="btn btn-secondary"  (click)="find()" type="button">Go!</button>
-    </span>
-  </div>
-</div>
-</div> 
-</form>
-       
-    <div class="container">
-    <table class="table" *ngIf="movietodisplay.length > 0">
-      <tr>
-        <th>Id</th>
-        <th>Title</th>
-        <th>Year</th>
-        <th>Duration</th>
-        <th>Owner</th>
-        <th>Rent</th>
-      </tr>
-      <tr *ngFor="let movie of movietodisplay">
-      <td>{{movie.id}}</td>
-      <td>{{movie.title}}</td>
-      <td>{{movie.year}}</td>
-      <td>{{movie.duration}}</td>
-      <td>{{movie.owner}}</td>
-      <td><a href="">Rent</a></td>
-     </tr>
-    </table>
-    </div>
-
-    <form #form="ngForm" (ngSubmit)="submit(form)">
-    
-        <div class="form-group">
-        <label for="movie_title">Movie Title</label>
-        <input [(ngModel)]= "movietoadd.title"  name="movie_title" #MovieTitle="ngModel" id="movie_title" type="text" class="form-control">         
-        </div> 
-        <div class="form-group">
-        <label for="movie_year">Movie Year</label>
-        <input [(ngModel)]= "movietoadd.year"  name="movie_year" #MovieYear="ngModel" id="year" type="text" class="form-control">         
-        </div> 
-        <div class="form-group">
-        <label for="movie_duration">Movie Duration</label>
-        <input [(ngModel)]= "movietoadd.duration"  name="movie_duration" #MovieDuration="ngModel" id="duration" type="text" class="form-control">         
-        </div>  
-        
-        <button class="btn btn-primary">Add Movie</button>
-    </form >
-
-    
-  `,
+  templateUrl: './movie-template.html'   
+  ,
   styles: []
 })
 export class MovieComponent {
@@ -82,6 +18,13 @@ export class MovieComponent {
    searchtitle: String='';
    movietoadd: Movie = new Movie();
    movietodisplay: Movie[]=[];
+   moviesearched: Movie[]=[]; 
+   results: string[];
+   constructor(private http: HttpClient) {}
+   
+   
+    
+
    listMovie: Movie[] = [
     { 
         id: 1,
@@ -124,8 +67,16 @@ export class MovieComponent {
         console.log(this.searchtitle);
         this.listMovie.forEach(movie=>{
             if(movie.title==this.searchtitle){
-                this.movietodisplay.push(movie);
+                this.moviesearched.push(movie);
             }
+        });
+        this.movietodisplay= this.moviesearched;
+    }
+
+    displayall(){
+        this.http.get('http://localhost:3001/movies').subscribe(data => {
+            // Read the result field from the JSON response.
+            console.log(data);
         });
     }
 }
